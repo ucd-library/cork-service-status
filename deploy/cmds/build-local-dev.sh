773c2dd92e542d67fa75d9373011eb912b8abc7c
spelkey-ucd <spelkey@ucdavis.edu>
2025-06-05 11:32:27 -0400

    setting up build

---
+++ deploy/cmds/build-local-dev.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  echo "Please provide a version number"
+  exit 1
+fi
+
+DEPTH=$2
+if [ -z "$DEPTH" ]; then
+  DEPTH=ALL
+fi
+
+cork-kube build exec \
+  --project cork-service-status \
+  --version $VERSION \
+  --override-tag local-dev \
+  --depth $DEPTH \
+  --no-cache-from
